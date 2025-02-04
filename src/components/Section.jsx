9750540c621cdce3e1852a6d24980812fceeed41
RiccardoAD <rick_95@libero.it>
2025-02-04 12:20:04 +0100

    creato pirma parte di componente Hero con parallax

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -4,8 +4,8 @@ const Section = ({ className, id, crosses, crossesOffset, customPaddings, childr
   return (
     <div
       id={id}
-      className={`relative ${
-        customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""} ${className || ""}`
+      className={`relative ${customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`} ${
+        className || ""
       }`}
     >
       {children}
