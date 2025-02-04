afd3fda3dba3637249f9905074108cfcbf6997bf
RiccardoAD <rick_95@libero.it>
2025-02-04 15:13:59 +0100

    add benefits

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 // import Button from "./components/Button";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-
+import Benefits from "./components/Benefits";
 const App = () => {
   return (
     <>
@@ -10,9 +10,10 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         {/* <Button className="mt-10" href="#login">
           Something
-        </Button> */}
+          </Button> */}
         <Header />
         <Hero />
+          <Benefits />
       </div>
       <ButtonGradient />
     </>
